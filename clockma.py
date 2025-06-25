488761e8a3325f5e46a0087c3dcdb865f95a0356
alexfirerain <alexfirerain@yandex.ru>
2025-06-25 16:24:25 +0300

    output formatting full

--- clockma.py
+++ clockma.py
@@ -20,22 +20,36 @@
 #
 #     print(greeting)
 # print('Пока)')
+#
+# while True:
+#     word = input('Введите слово, не короче четырёх букв: ')
+#     if not word : print('а иди-ка ты')
+#     if word == 'stop' or word == 'хватит': break
+#     if len(word) < 4:
+#         print('не годится')
+#     else:
+#         print('спасибо, слово "' + word + '" имеет длину', len(word))
+#
+# word1 = 'veni'
+# word2 = 'vidi'
+# word3 = 'vici'
+# word4 = '29\xb0Ц'
+#
+# print(word1, word2, word3, sep=', ', end=' -> ')
+# print(word4, end='')
+# print('концерт группы \'Озон\'')
+# print('управляющая \nпоследовательность \\n')
 
-while True:
-    word = input('Введите слово, не короче четырёх букв: ')
-    if not word : print('а иди-ка ты')
-    if word == 'stop' or word == 'хватит': break
-    if len(word) < 4:
-        print('не годится')
-    else:
-        print('спасибо, слово "' + word + '" имеет длину', len(word))
+name = 'Васудэва'
+email = 'vasudeva@mahabharata.com'
+age = 33
+weight = 75.8897
 
-word1 = 'veni'
-word2 = 'vidi'
-word3 = 'vici'
-word4 = '29\xb0Ц'
-
-print(word1, word2, word3, sep=', ', end=' -> ')
-print(word4, end='')
-print('концерт группы \'Озон\'')
-print('управляющая \nпоследовательность \\n')
+#1 метод плейсхолдера:
+print('Имя: %s, мыло: %s, возраст: %d' % (name, email, age))
+#2 позиционных
+#3 именованных аргументов
+#4 метод формат:
+print('Имя: {}, мыло: {}, возраст: {}'.format(name, email, age))
+#5 Ф-строка:
+print(f'Имя: {name:.6}, мыло: {email}, возраст: {age}, вес: {weight:.2f}')
