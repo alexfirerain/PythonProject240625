ccd4f63e8ad65ccae8e6b0f066e5a1e203bb4a55
ЛЦИМС 0 2 <LCIMS2@edu.spbgasu.ru>
2025-06-25 10:14:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# print("я покину Индрапрастху на четырнадцать дней")
+# print('едва ли паломничество будет длинней')
+#
+#
+# def indraprastha_react():
+#     print('\nне все будут рады')
+#
+#
+# indraprastha_react()
+# print('на площади играет группа "Кино"')
+# print('Привет, Сахадева')
+# name = 'Накула'  # string
+# name2 = 'Бхима'
+# print('Привет, ' + name)
+# age = 29  # int
+# print('Привет, ', name, ',', name2, 27, age)
+# temper = 24.7  # float
+#
+# petrovName = 'Сергей'
+# petrovSurname = 'Петров'
+# petrovAge = 27
+#
+# print('Привет,', petrovName, petrovSurname, ', тебе', petrovAge, 'лет.')
+# print('На улице температура', temper, '°')
+#
+# name = input('Новое имя: ')
+# print('Привет,', name, '!')
+#
+# mine = int(input('Сколько мне: '))
+# yours = int(input('Сколько тебе: '))
+# print('Нам', mine + yours, 'в совокупности')
+
+a = 23.88
+print(round(a, 1))
+
+screen = 1280 // 7
+print(screen)
+
+# number = int(input('Введи число: '))
+# print('Число', number, 'заканчивается на',
+#       number % 10)
+"""
+Многострочный комментарий
+    или многострочная строка (текстовой блок),
+    т.е. может быть присвоена переменной
+"""
+
+value = int(input('Введи число: '))
+print('Число ', value, 'в степени 3 =', value ** 3)
+print('Квадратность от ', value, '=', int(value ** (1 / 3)))
+print('Квадратность от ', value, '=', round(value ** (1 / 3)))
