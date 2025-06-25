eda9f649f5369bf7a291a966c8b44e8102fc15d0
alexfirerain <alexfirerain@yandex.ru>
2025-06-25 15:15:45 +0300

    length definer added

---
+++ clockma.py
@@ -0,0 +1,31 @@
+# while True:
+#     user_answer = input('Который час?\n')
+#
+#     if user_answer == "пока" or user_answer == "bye":
+#         break
+#
+#     hour = int(user_answer)
+#
+#     if hour > 23: hour = 23
+#     if hour < 0: hour = 0
+#
+#     if 7 <= hour <= 11:
+#         greeting = "Доброго утречка!"
+#     elif 11 < hour <= 18:
+#         greeting = "Доброго дня!"
+#     elif 18 < hour <= 22:
+#         greeting = "Доброго вечера!"
+#     else:
+#         greeting = "Доброй ночи!"
+#
+#     print(greeting)
+# print('Пока)')
+
+while True:
+    word = input('Введите слово, не короче четырёх букв: ')
+    if not word : print('а иди-ка ты')
+    if word == 'stop' or word == 'хватит': break
+    if len(word) < 4:
+        print('не годится')
+    else:
+        print('спасибо, слово "' + word + '" имеет длину', len(word))
