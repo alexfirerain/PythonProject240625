01d22e51f89f6ebb8669af40035762b37ec4de63
alexfirerain <alexfirerain@yandex.ru>
2025-06-27 14:07:43 +0300

    коллекции

--- minimax.py
+++ minimax.py
@@ -22,14 +22,33 @@ print(f'Максимум: {max_n}')
 print(f'Минимум: {min_n}')
 
 ##############
+coins = [5, 4, 5]
 
-def fake_check(coin_a, coin_b, coin_c):
-    if coin_a == coin_b:
-        return coin_c
-    if coin_a > coin_b:
-        return coin_c
-    else:
-        return coin_a
+# def fake_check(coin_set):
+#     if coin_set[0] == coin_set[1]:
+#         return 3
+#     if coin_set[0] > coin_set[1]:
+#         return 2
+#     return 1
 
-print(fake_check(3, 2, 3))
+def fake_check(coin_set):
+    return 3 if coin_set[0] == coin_set[1]\
+        else 2 if coin_set[0] > coin_set[1]\
+        else 1
+
+
+# def fake_check(coin_a, coin_b, coin_c):
+#     if coin_a == coin_b:
+#         return coin_c
+#     if coin_a > coin_b:
+#         return coin_c
+#     return coin_a
+
+# print(fake_check(3, 2, 3))
+
+
+print('Из этих монет фейковой является монета №' + str(fake_check(coins)))
+
+
+
 
